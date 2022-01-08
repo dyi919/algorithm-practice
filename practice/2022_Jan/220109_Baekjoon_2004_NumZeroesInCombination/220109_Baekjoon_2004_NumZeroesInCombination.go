4c0d0ab6cf2de1420d0205d76578220c29c9860b
dyi919 <ehddjs13579@gmail.com>
2022-01-09 01:51:28 +0900

    220109_Baekjoon_2004_NumZeroesInCombination
    Number Theory & Combinatorics

---
+++ practice/2022_Jan/220109_Baekjoon_2004_NumZeroesInCombination/220109_Baekjoon_2004_NumZeroesInCombination.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var scanner = bufio.NewScanner(os.Stdin)
+var w = bufio.NewWriter(os.Stdout)
+
+func main() {
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+	scanner.Split(bufio.ScanWords)
+	n, m := nextInt(), nextInt()
+	fiveCnt := powerCnt(n, 5) - powerCnt((n-m), 5) - powerCnt(m, 5)
+	twoCnt := powerCnt(n, 2) - powerCnt((n-m), 2) - powerCnt(m, 2)
+	fmt.Fprint(w, min(fiveCnt, twoCnt))
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func powerCnt(n, pow int) int {
+	cnt := 0
+
+	for n >= pow {
+		cnt += n / pow
+		n /= pow
+	}
+
+	return cnt
+}
+
+func nextInt() int {
+	scanner.Scan()
+	r := 0
+	for _, c := range scanner.Bytes() {
+		r *= 10
+		r += int(c - '0')
+	}
+	return r
+}
