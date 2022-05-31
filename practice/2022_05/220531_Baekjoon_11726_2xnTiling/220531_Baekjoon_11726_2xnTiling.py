0aba6e6b8a45068296b40a7d07b0fdc9c34bab0d
Dongun Yi <ydu0919@gmail.com>
2022-05-31 23:29:50 +0900

    220531

---
+++ practice/2022_05/220531_Baekjoon_11726_2xnTiling/220531_Baekjoon_11726_2xnTiling.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/11726
+
+MOD = 10007
+
+n = int(input())
+
+cache = [0] * (n+1)
+cache[1] = 1
+cache[2] = 2
+
+for i in range(3, n+1):
+    cache[i] = (cache[i-1] + cache[i-2]) % MOD
+
+print(cache[n])
