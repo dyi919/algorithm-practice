d6ed823173dad12329f1ff1aff12e8b2dab84ade
dyi919 <ehddjs13579@gmail.com>
2022-09-18 23:59:11 +0900

    220918

---
+++ practice/2022_09/220918_Baekjoon_18352_FindCityWithDistance/220918_Baekjoon_18352_FindCityWithDistance.py
@@ -0,0 +1,38 @@
+from sys import stdin
+from collections import deque
+input = stdin.readline
+INF = int(1e9)
+
+N, M, K, X = map(int, input().split())
+edges = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+ans = []
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    edges[a].append(b)
+    edges[b].append(a)
+
+queue = deque()
+visited[X] = True
+queue.append((X, 0))
+
+while queue:
+    v, c = queue.popleft()
+
+    if c == K:
+        ans.append(v)
+        continue
+
+    for nv in edges[v]:
+        if not visited[nv]:
+            visited[nv] = True
+            queue.append((nv, c+1))
+
+ans.sort()
+
+if len(ans) == 0:
+    print(-1)
+else:
+    for a in ans:
+        print(a)
