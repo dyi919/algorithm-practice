167c99c8d2082283c25a50f1f2b26d96af6f279d
dyi919 <ehddjs13579@gmail.com>
2022-12-16 02:29:24 +0900

    221216

--- practice/2022_12/221216_Programmers_JadenCase/221216_Programmers_JadenCase.js
+++ practice/2022_12/221216_Programmers_JadenCase/221216_Programmers_JadenCase.js
@@ -1,3 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12951
+
 function convertToJadenCase(word) {
   const firstLetter = word.slice(0, 1).toUpperCase();
   const restLetters = word.slice(1).toLowerCase();
