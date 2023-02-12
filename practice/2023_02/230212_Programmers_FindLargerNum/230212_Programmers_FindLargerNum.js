7d19c1ebed32c478cc3a804e38d92c6680320193
dyi919 <ehddjs13579@gmail.com>
2023-02-12 16:11:48 +0900

    230212

---
+++ practice/2023_02/230212_Programmers_FindLargerNum/230212_Programmers_FindLargerNum.js
@@ -0,0 +1,29 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/154539
+
+function solution(numbers) {
+  let answer = Array(numbers.length).fill(-1);
+  let stack = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (stack.length === 0) {
+      stack = [i];
+      answer[i] = -1;
+      continue;
+    }
+
+    while (stack.length > 0) {
+      const idx = stack.pop();
+
+      if (numbers[i] > numbers[idx]) {
+        answer[idx] = numbers[i];
+      } else {
+        stack.push(idx);
+        break;
+      }
+    }
+
+    stack.push(i);
+  }
+
+  return answer;
+}
