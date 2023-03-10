b7eced05a5f6e1b2af0fa1e395752c445bfe2e43
dyi919 <ehddjs13579@gmail.com>
2023-03-10 23:58:43 +0900

    230310

---
+++ practice/2023_03/230310_LeetCode_NonDecreasingArray/230310_LeetCode_NonDecreasingArray.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/non-decreasing-array/description/
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+let checkPossibility = function (nums) {
+  let count = 1;
+  let i = 1;
+
+  while (i < nums.length) {
+    if (nums[i - 1] > nums[i]) {
+      if (count > 0) {
+        count--;
+        if (i > 1 && nums[i - 2] > nums[1]) nums[i] = nums[i - 1];
+      } else return false;
+    }
+    i++;
+  }
+
+  return true;
+};
