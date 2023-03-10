6219aa9413ff5230b711f0a07b25a10761ebbe03
dyi919 <ehddjs13579@gmail.com>
2023-03-11 00:02:00 +0900

    230310

--- practice/2023_03/230310_LeetCode_NonDecreasingArray/230310_LeetCode_NonDecreasingArray.js
+++ practice/2023_03/230310_LeetCode_NonDecreasingArray/230310_LeetCode_NonDecreasingArray.js
@@ -12,7 +12,7 @@ let checkPossibility = function (nums) {
     if (nums[i - 1] > nums[i]) {
       if (count > 0) {
         count--;
-        if (i > 1 && nums[i - 2] > nums[1]) nums[i] = nums[i - 1];
+        if (i > 1 && nums[i - 2] > nums[i]) nums[i] = nums[i - 1];
       } else return false;
     }
     i++;
