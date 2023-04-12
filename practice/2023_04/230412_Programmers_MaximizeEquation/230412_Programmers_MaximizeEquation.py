f9d11ff34861b9c4a1c0fed9f2a6d21ce5978b34
dyi919 <ehddjs13579@gmail.com>
2023-04-12 23:56:24 +0900

    230412

---
+++ practice/2023_04/230412_Programmers_MaximizeEquation/230412_Programmers_MaximizeEquation.py
@@ -0,0 +1,15 @@
+import re
+def solution(expression):
+    answer = []
+    combinations = [['+','-','*'],['+','*','-'],['-','+','*'],['-','*','+'],['*','-','+'],['*','+','-']]
+    for combination in combinations:
+        operand = re.split('[*+-]',expression)
+        operator = re.split('[0-9]+',expression)[1:-1]
+        for comb in combination:
+            while comb in operator:
+                idx = operator.index(comb)
+                operand[idx] = str(eval(operand[idx] + comb + operand[idx+1]))
+                del operand[idx+1]
+                del operator[idx]
+        answer.append(abs(int(operand[0])))
+    return max(answer)
